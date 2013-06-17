ee798fa98eb74cba76c3bf7f9c714dbd6f202717
Kjeld Froberg <kfr@trifork.com>
2013-06-17 13:34:16 +0200

    Tilføjet infrastruktur

---
+++ src/main/java/dk/nsi/haiba/epimibaimporter/dao/impl/HAIBADAOImpl.java
@@ -0,0 +1,46 @@
+/**
+ * The MIT License
+ *
+ * Original work sponsored and donated by National Board of e-Health (NSI), Denmark
+ * (http://www.nsi.dk)
+ *
+ * Copyright (C) 2011 National Board of e-Health (NSI), Denmark (http://www.nsi.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package dk.nsi.haiba.epimibaimporter.dao.impl;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import dk.nsi.haiba.epimibaimporter.dao.CommonDAO;
+import dk.nsi.haiba.epimibaimporter.dao.HAIBADAO;
+import dk.nsi.haiba.epimibaimporter.log.Log;
+
+public class HAIBADAOImpl extends CommonDAO implements HAIBADAO {
+
+	private static Log log = new Log(Logger.getLogger(HAIBADAOImpl.class));
+
+	@Autowired
+	@Qualifier("haibaJdbcTemplate")
+	JdbcTemplate jdbc;
+
+}
